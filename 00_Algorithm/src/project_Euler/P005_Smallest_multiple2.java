7523c4c8489f21ec4241fec528b83d265f002522
한창민 <enigmatic_duke@naver.com>
2016-08-09 15:41:02 +0900

    주석 해쉬맵 제거함. 깔끔
    깔끔.

--- 00_Algorithm/src/project_Euler/P005_Smallest_multiple2.java
+++ 00_Algorithm/src/project_Euler/P005_Smallest_multiple2.java
@@ -1,10 +1,6 @@
 package project_Euler;
 
-import java.util.HashMap;
-
 public class P005_Smallest_multiple2 {
-	
-	static HashMap<Integer, Integer> store = new HashMap<Integer, Integer>();
 	
 	static int[] checkIn=new int[20];
 	static int[] total=new int[20];
@@ -27,20 +23,16 @@ public class P005_Smallest_multiple2 {
 				checkIn[div-1]+=1;
 				div=2;
 				
-				//System.out.println("실행:"+temp);
 			}else{
 				div++;
 			}
-			
 		}
 	}//end function
 	
 	public static void checkTotal(){
 		
 		for(int i=0;i<20;i++){
-		if(checkIn[i] == total[i] || checkIn[i] < total[i]){
-			//System.out.println("ㅇㅋ");
-		}else if(checkIn[i] > total[i]){
+		if(checkIn[i] > total[i]){
 			total[i]+=(checkIn[i]-total[i]);
 		}
 		
@@ -49,21 +41,14 @@ public class P005_Smallest_multiple2 {
 	}//end function
 	
 	public static void main(String[] args) {
-		
-		
-		//setCheck(20); //어떤 숫자의 인수를 배열에 저장하는 함수
-		
+
 		for(int i = 20; i>1;i--){
 			setCheck(i);
 			checkTotal();
 		}
 		
 		int multiple=1;
-		//System.out.print("값=");
 		for(int i=0;i<20;i++){
-			//System.out.print("["+(i+1)+"]=");
-			//System.out.print(total[i]);
-			//System.out.print("개 ");
 			for(int j=0;j<total[i];j++){
 				multiple*=(i+1);
 			}
